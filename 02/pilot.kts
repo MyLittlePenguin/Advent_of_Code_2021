10eb7281688278402b332e4c292de1097450157d
AV01V42 <ronny.krone@volkswagen-groupservices.com>
2021-12-02 13:35:33 +0100

    refactoring

--- 02/pilot.kts
+++ 02/pilot.kts
@@ -1,40 +1,25 @@
-import java.io.File
-
-data class Vec(var first: Int, var second: Int) {
-    operator fun plus(other: Vec) = Vec(this.first + other.first, this.second + other.second)
+data class Vec(var distance: Int, var depth: Int, var aim: Int = 0) {
+    operator fun plus(other: Vec) = Vec(this.distance + other.distance, this.depth + other.depth)
 }
 
-fun readInstructions(instruction: String): Vec {
-    val op = instruction.split(" ").let { Pair<String, Int>(it.first(), it[1].toInt()) }
-    return when(op.first) {
-        "forward" -> Vec(op.second, 0)
-        "down" -> Vec(0, op.second)
-        "up" -> Vec(0, -(op.second))
-        else -> Vec(0, 0)
+fun List<String>.readInstructions(): List<Vec> = this.map {
+        val op = it.split(" ").let { Pair<String, Int>(it[0], it[1].toInt()) }
+        when (op.first) {
+            "forward" -> Vec(op.second, 0)
+            "down" -> Vec(0, op.second)
+            "up" -> Vec(0, -(op.second))
+            else -> Vec(0, 0)
+        }
     }
-}
 
-inline fun calcNewPosProduct(input: List<String>, mapper: (Vec) -> Vec = { it }) =
-    input.map { readInstructions(it) }.map(mapper).reduce { acc, unit -> acc + unit }.let { it.first * it.second }
+fun List<Vec>.calcNewPosProduct(): Int = this.reduce { acc, unit -> acc + unit }.let { it.distance * it.depth }
 
-fun calcNewPosProduct2(input: List<String>): Int {
-    var aim = 0
-    return calcNewPosProduct(input) {
-        aim += it.second
-        Vec(it.first, aim * it.first)
-    }
-}
+fun List<Vec>.calcNewPosProduct2(): Int = this.windowed(2, 1, true) {
+        if(it.size == 2) it[1].aim = it[0].aim + it[1].depth
+        it[0].apply { depth = it[0].distance * it[0].aim }
+    }.calcNewPosProduct()
 
-val input = File("02/input.txt").readLines(Charsets.UTF_8)
+val input = java.io.File("02/input.txt").readLines(Charsets.UTF_8).readInstructions()
 
-println(
-    calcNewPosProduct(input)
-)
-
-println(
-    calcNewPosProduct2(listOf("forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"))
-)
-
-println(
-    calcNewPosProduct2(input)
-)
+println( input.calcNewPosProduct() )
+println( input.calcNewPosProduct2() )
