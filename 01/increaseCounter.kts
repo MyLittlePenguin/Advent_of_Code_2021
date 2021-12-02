00bbbdfb44e0ab4e19ce2a45fdab02f5b803f727
AV01V42 <ronny.krone@volkswagen-groupservices.com>
2021-12-02 11:37:15 +0100

    refactoring

--- 01/increaseCounter.kts
+++ 01/increaseCounter.kts
@@ -1,9 +1,9 @@
-fun List<Int>.incCounter() = this.windowed(2, 1).map { if(it[0] < it[1]) 1 else 0 }.sum()
+fun List<Int>.incCounter() = this.windowed(2, 1) { if(it[0] < it[1]) 1 else 0 }.sum()
 
 val input = java.io.File("01/input.txt").readLines(Charsets.UTF_8).map { it.toInt() }
 
 println(input.incCounter())
 
 println(
-    input.windowed(3, 1).map { it.sum() }.incCounter()
+    input.windowed(3, 1) { it.sum() }.incCounter()
 )
