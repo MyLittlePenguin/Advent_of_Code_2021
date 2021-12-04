46e1e3e4724b9bd5a3a2d18d03f598e0afb0c302
Ronny Krone <r.krone.rk@gmail.com>
2021-12-04 18:30:53 +0100

    refactoring

--- 04/bingo.kts
+++ 04/bingo.kts
@@ -1,22 +1,18 @@
-val input = java.io.File("04/input.txt").readLines()
+val input = java.io.File("04/input.txt").readLines().toMutableList()
 
 data class BingoNum(val num: Int, var marked: Boolean = false)
-data class Board(val fields: Array<BingoNum>, var won: Boolean = false) {
+data class Board(val fields: List<BingoNum>, var won: Boolean = false) {
     fun calcResult(winningNum: Int) = this.fields.map { if (it.marked) 0 else it.num }.sum().let { it * winningNum }
 }
 
-val iter = input.iterator()
-val nSeq = iter.next().split(",").map { it.toInt() }
-var boards: List<Board> = listOf()
+val nSeq = input.first().split(",").map { it.toInt() }
+input.removeFirst()
 
-do {
-    iter.next()
-    var board: Array<BingoNum> = arrayOf()
-    repeat(5) {
-        board += iter.next().trim().split("  ", " ").map { BingoNum(it.toInt()) }
-    }
-    boards += Board(board)
-} while (iter.hasNext())
+var boards = input.windowed(6, 6).map {
+    Board(it.slice(1..5).map {
+        it.trim().split("  ", " ").map { BingoNum(it.toInt()) }
+    }.flatten())
+}
 
 var result1 = -1
 var result2 = -2
