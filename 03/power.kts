0b7fa99ad5487d89b3839fa91a0f8c8ee73315ac
Ronny Krone <r.krone.rk@gmail.com>
2021-12-04 16:26:54 +0100

    refactoring

--- 03/power.kts
+++ 03/power.kts
@@ -2,26 +2,22 @@ val input = java.io.File("03/input.txt").readLines()
 var inputO2 = java.io.File("03/input.txt").readLines()
 var inputCo2 = java.io.File("03/input.txt").readLines()
 
-fun List<String>.countAtPos(i: Int) = this.groupingBy { it[i] }.eachCount()
 fun Char.filterListe(liste: List<String>, pos: Int): List<String> = liste.filter { it[pos] == this }
 fun String.b2i() = Integer.parseInt(this, 2)
 
-fun Map<Char, Int>.evalZeros(commonZero: Char, commonOne: Char) = when {
-    this.size == 1 -> this.keys.first()
-    this['0']!! > this['1']!! -> commonZero
-    else -> commonOne
-}
+fun Int.isCommon(size: Int) = if(size - this > this) '0' else '1'
+fun Int.notCommon(size: Int) = if(size - this > this) '1' else '0'
 
 var gamma = ""
 var epsilon = ""
 
 for(i in 0 until input[0].length) {
-    input.countAtPos(i).let {
-        gamma += it.evalZeros('0', '1')
-        epsilon += it.evalZeros('1', '0')
+    input.count { it[i] == '1' }.let {
+        gamma += it.isCommon(input.size)
+        epsilon += it.notCommon(input.size)
     }
-    inputO2 = inputO2.countAtPos(i).evalZeros('0', '1').filterListe(inputO2, i)
-    inputCo2 = inputCo2.countAtPos(i).evalZeros('1', '0').filterListe(inputCo2, i)
+    if(inputO2.size > 1) inputO2 = inputO2.count { it[i] == '1' }.isCommon(inputO2.size).filterListe(inputO2, i)
+    if(inputCo2.size > 1) inputCo2 = inputCo2.count { it[i] == '1' }.notCommon(inputCo2.size).filterListe(inputCo2, i)
 }
 
 println("Energy: " + gamma.b2i() * epsilon.b2i())
