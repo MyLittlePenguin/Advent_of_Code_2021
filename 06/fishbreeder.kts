9f57183340c1b09b0263376bed987ef3981491c3
AV01V42 <ronny.krone@volkswagen-groupservices.com>
2021-12-06 08:40:40 +0100

    06

---
+++ 06/fishbreeder.kts
@@ -0,0 +1,17 @@
+data class Fish(var state: Long, var count: Long)
+
+val input = java.io.File("06", "input.txt").readText().trim().split(",").groupingBy { it.toInt() }.eachCount()
+var fishswarm = input.entries.map { Fish(it.key.toLong(), it.value.toLong()) }
+
+repeat(256) {
+    fishswarm.forEach {
+        it.state--
+    }
+    fishswarm += fishswarm.filter { it.state == -1L }.map { it.state = 6; Fish(8, it.count) }
+    fishswarm = fishswarm.groupBy { it.state }.values.map {
+        it.reduce { acc, fish ->
+            Fish(acc.state, acc.count + fish.count)
+        }
+    }
+}
+println(fishswarm.sumOf { it.count })
