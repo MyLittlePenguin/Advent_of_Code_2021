07618aa910ba2b1e2854dd63de3fd2f523e46dc2
AV01V42 <ronny.krone@volkswagen-groupservices.com>
2021-12-09 08:40:36 +0100

    refactoring

--- 09/solution.kts
+++ 09/solution.kts
@@ -7,7 +7,6 @@ for(row in 0 until input.size) {
     for(col in 0 until input[row].size) {
         var up = if(row == 0) true else input[row][col] < input[row - 1][col]
         var down = if(row == input.size - 1) true else input[row][col] < input[row + 1][col]
-        
         var left = if(col == 0) true else input[row][col] < input[row][col - 1]
         var right = if(col == input[0].size - 1) true else input[row][col] < input[row][col + 1]
         
@@ -18,19 +17,14 @@ println(lowPoints.sumOf { input[it.row][it.col] + 1 })
 
 fun checkBasin(seed: Low): Int {
     var (row, col) = seed
-    if(input[row][col] >= 9) return -1
-    
-    var up = if(row == 0 || input[row - 1][col] >= 9 || input[row][col] > input[row - 1][col]) 0 else 1
-    var down = if(row == input.size - 1 || input[row + 1][col] >= 9 || input[row][col] > input[row + 1][col]) 0 else 1
-    var right = if(col == input[row].size - 1 || input[row][col + 1] >= 9 || input[row][col] > input[row][col + 1]) 0 else 1
-    var left = if(col == 0 || input[row][col - 1] >= 9 || input[row][col] > input[row][col - 1]) 0 else 1
-    
+    val current = input[row][col]
+    val checkField = { it: Int -> it >= 9 || current > it }
     input[row][col] = input[row][col] or 0b1000000
-    if(up == 1 && row > 0) up += checkBasin(Low(row - 1, col))
-    if(down == 1 && row < input.size - 1) down += checkBasin(Low(row + 1, col))
-    if(left == 1 && col > 0) left += checkBasin(Low(row, col - 1))
-    if(right == 1 && col < input[row].size - 1) right += checkBasin(Low(row, col + 1))
-    return up + down + left + right
+    
+    return (if(row == 0 || checkField(input[row - 1][col])) 0 else 1 + checkBasin(Low(row - 1, col))) +
+        (if(row == input.size - 1 || checkField(input[row + 1][col])) 0 else 1 + checkBasin(Low(row + 1, col))) +
+        (if(col == input[row].size - 1 || checkField(input[row][col + 1])) 0 else 1 + checkBasin(Low(row, col + 1))) +
+        (if(col == 0 || checkField(input[row][col - 1])) 0 else 1 + checkBasin(Low(row, col - 1)))
 }
 
 lowPoints.map { 1 + checkBasin(it) }.sortedDescending().let { println(it[0] * it[1] * it[2]) }
